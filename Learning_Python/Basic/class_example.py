db4d7bb2dcc16147dc3cb32d45dc27084a6d92b7
anddy.liu <lqdflying@gmail.com>
2020-04-19 21:27:36 +0800

    2020-4-19 21:27:34

---
+++ Learning_Python/Basic/class_example.py
@@ -0,0 +1,62 @@
+# -*- coding:utf-8 -*-
+###
+# File: class_example.py
+# Created Date: 2020-04-19
+# Author: anddy.liu
+# Contact: <lqdflying@gmail.com>
+# 
+# Last Modified: Sunday April 19th 2020 7:29:54 pm
+# 
+# Copyright (c) 2020 personal
+# <<licensetext>>
+# -----
+# HISTORY:
+# Date      	 By	Comments
+# ----------	---	----------------------------------------------------------
+###
+
+class MyClass:
+   """一个简单的类实例"""
+   i = 12345
+   def f(self):
+       return 'hello world'
+# 实例化类
+x = MyClass()
+#we created an object by adding parentheses to the name of the class. We then assigned that new instance to the variable cool_instancefor safe-keeping.
+#类名MyClass后边加括号是真实的在内存中实例化一个类的操作,赋值给前边的x变量是为了safe-keeping(保证后边可以引用这个实例化类)
+# 访问类的属性和方法
+print("MyClass 类的属性 i 为：", x.i)
+print("MyClass 类的方法 f 输出为：", x.f())
+
+class Complex:
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
+x = Complex(3.0, -4.5)
+print(x.r, x.i)
+
+class circle1:
+    pi = 3.14
+    def __init__(self,diameter,myname):
+        print("New circle1 with diameter:  {diameter}".format(diameter = diameter))
+#由于构造函数里有print动作,所以下边的类的实例化操作会触发动作,进而导致print的输出
+teaching_table1 = circle1(36, "liuqd")
+#print(teaching_table1.diameter) 
+#上边这个print会报错:'circle1' object has no attribute 'diameter'
+class circle2:
+    pi = 3.14
+    def __init__(self,diameter,myname):
+        self.diameter = diameter
+        self.myname = myname
+        print("New circle2 with diameter:  {diameter}".format(diameter = diameter))
+
+teaching_table2 = circle2(36, "liuqd")
+print(teaching_table2.diameter)
+
+class Employee:
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return self.name
+argus = Employee("Argus Filch")
+print(argus)
