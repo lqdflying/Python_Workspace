7c980f01af4e388fa3f2a20ba5a869f2f0a1be97
anddy.liu <lqdflying@gmail.com>
2020-04-27 23:06:01 +0800

    2020-4-27 23:05:59

---
+++ Learning_Python/namespace_package2/forfun/second/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+###
+# File: __init__.py
+# Created Date: 2020-04-27
+# Author: anddy.liu
+# Contact: <lqdflying@gmail.com>
+# 
+# Last Modified: Monday April 27th 2020 11:05:45 pm
+# 
+# Copyright (c) 2020 personal
+# <<licensetext>>
+# -----
+# HISTORY:
+# Date      	 By	Comments
+# ----------	---	----------------------------------------------------------
+###
---
+++ Learning_Python/namespace_package2/forfun/second/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+###
+# File: __main__.py
+# Created Date: 2020-04-27
+# Author: anddy.liu
+# Contact: <lqdflying@gmail.com>
+# 
+# Last Modified: Monday April 27th 2020 11:05:54 pm
+# 
+# Copyright (c) 2020 personal
+# <<licensetext>>
+# -----
+# HISTORY:
+# Date      	 By	Comments
+# ----------	---	----------------------------------------------------------
+###
