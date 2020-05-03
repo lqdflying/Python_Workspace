d178c0974b739e1437d5a147692f65a0b1024f6b
anddy.liu <lqdflying@gmail.com>
2020-05-03 19:26:34 +0800

    2020-5-3 19:26:32

--- Learning_Python/Basic/socket_simple_server.py
+++ Learning_Python/Basic/socket_simple_server.py
@@ -5,7 +5,7 @@
 # Author: anddy.liu
 # Contact: <lqdflying@gmail.com>
 # 
-# Last Modified: Sunday May 3rd 2020 4:35:38 pm
+# Last Modified: Sunday May 3rd 2020 7:25:20 pm
 # 
 # Copyright (c) 2020 personal
 # <<licensetext>>
@@ -20,15 +20,16 @@ server = socket.socket() #获得socket实例
 
 server.bind(("localhost",10999)) #绑定ip port
 server.listen()  #开始监听
-print("等待客户端的连接...")
-conn,addr = server.accept() #接受并建立与客户端的连接,程序在此处开始阻塞,只到有客户端连接进来...
-print("新连接:",addr )
 while True:
-    data = conn.recv(1024)
-    if not data:
-        print("客户端断开了...")
-        break
-    print("收到消息:",data.decode("utf-8"))
-    msg = "服务器返回消息: %s"%data.decode("utf-8")
-    conn.send(msg.encode("utf-8")) #服务器端发送消息
+    print("等待客户端的连接...")
+    conn,addr = server.accept() #接受并建立与客户端的连接,程序在此处开始阻塞,只到有客户端连接进来...
+    print("新连接:",addr )
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            print("客户端断开了...")
+            break
+        print("收到消息:",data.decode("utf-8"))
+        msg = "服务器返回消息: %s"%data.decode("utf-8")
+        conn.send(msg.encode("utf-8")) #服务器端发送消息
 server.close()
