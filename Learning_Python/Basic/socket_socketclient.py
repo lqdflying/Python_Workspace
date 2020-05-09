1094596ae7806659674c6a6b1c84c87cb2ad213a
anddy.liu <lqdflying@gmail.com>
2020-05-09 15:40:37 +0800

    2020-5-9 15:40:34

--- Learning_Python/Basic/socket_socketclient.py
+++ Learning_Python/Basic/socket_socketclient.py
@@ -5,7 +5,7 @@
 # Author: anddy.liu
 # Contact: <lqdflying@gmail.com>
 # 
-# Last Modified: Saturday May 9th 2020 11:00:51 am
+# Last Modified: Saturday May 9th 2020 11:03:40 am
 # 
 # Copyright (c) 2020 personal
 # <<licensetext>>
@@ -18,20 +18,24 @@ import socket
 
 HOST, PORT = "localhost", 9990
 # sock.connect((HOST, PORT))
-while True:
-    print("建立服务器端的连接")
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
+def main():
     while True:
-        data = input('Input [a-z]:>>')
-        if data == "bye":
-            print("按要求退出!")
-            sock.close()
-            exit()
-        sock.sendall(bytes(data + "\n", "utf-8"))
-        received = str(sock.recv(1024), "utf-8")
-        if not received: 
-            sock.close()
-            break
-        print("Sent:     {}".format(data))
-        print("Received: {}".format(received))
+        print("建立服务器端的连接")
+        sock = socket.socket()
+        sock.connect((HOST, PORT))
+        while True:
+            data = input('Input [a-z]:>>')
+            if data == "bye":
+                print("按要求退出!")
+                sock.close()
+                exit()
+            sock.sendall(bytes(data + "\n", "utf-8"))
+            received = str(sock.recv(1024), "utf-8")
+            if not received: 
+                sock.close()
+                break
+            print("Sent:     {}".format(data))
+            print("Received: {}".format(received))
+
+if __name__ == "__main__":
+    main()
--- Learning_Python/Basic/socket_socketserver.py
+++ Learning_Python/Basic/socket_socketserver.py
@@ -5,7 +5,7 @@
 # Author: anddy.liu
 # Contact: <lqdflying@gmail.com>
 # 
-# Last Modified: Saturday May 9th 2020 10:19:14 am
+# Last Modified: Saturday May 9th 2020 3:33:02 pm
 # 
 # Copyright (c) 2020 personal
 # <<licensetext>>
@@ -15,22 +15,32 @@
 # ----------	---	----------------------------------------------------------
 ###
 import socketserver
-class myserver(socketserver.BaseRequestHandler):
+# class myserver(socketserver.BaseRequestHandler):
+class myserver(socketserver.StreamRequestHandler): #继承StreamRequestHandler后类写法会变
     def handle(self):
-        print("新客户端连入--Ip:{0} Port:{1}".format(self.client_address[0], self.client_address[1]))
-        while True:
-            self.data = self.request.recv(1024).strip()
-            if not self.data:
-                self.request.sendall("输入不能为空".encode('utf8'))
-                continue
-            print(self.data.decode('utf8'))
-        # just send back the same data, but upper-cased
-            self.request.sendall(self.data.upper())
-    def finish(self):
-        print("客户端连接断开")
+        try:
+            print("新客户端连入--Ip:{0} Port:{1}".format(self.client_address[0], self.client_address[1]))
+            while True:
+                # self.data = self.request.recv(1024).strip()
+                self.data = self.rfile.readline().strip() #继承StreamRequestHandler后要用rfile
+                if not self.data:
+                    self.request.sendall("输入不能为空".encode('utf8'))
+                    continue
+                print(self.data.decode('utf8'))
+            # just send back the same data, but upper-cased
+                # self.request.sendall(self.data.upper())
+                self.wfile.write(self.data.upper()) #继承StreamRequestHandler后要用wfile
+        except BrokenPipeError:
+            print("客户端断开连接")
 
-if __name__ == "__main__":
+    def finish(self):
+        print("此处可以进行收尾的操作,比如关闭文件等")
+
+def main():
     HOST, PORT = "localhost", 9990
     server = socketserver.ThreadingTCPServer((HOST, PORT), myserver)    #实例化一个多线程TCPServer
     print('Wait client . . .') 
     server.serve_forever()
+
+if __name__ == "__main__":
+    main()
