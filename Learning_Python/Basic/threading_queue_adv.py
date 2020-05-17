be29e5e8c1d40e535f20af99b2cccf5bfa78c79b
anddy.liu <lqdflying@gmail.com>
2020-05-17 18:41:49 +0800

    2020-5-17 18:41:47

--- Learning_Python/Basic/threading_queue_adv.py
+++ Learning_Python/Basic/threading_queue_adv.py
@@ -5,7 +5,7 @@
 # Author: anddy.liu
 # Contact: <lqdflying@gmail.com>
 # 
-# Last Modified: Saturday May 16th 2020 11:29:08 pm
+# Last Modified: Sunday May 17th 2020 6:10:10 pm
 # 
 # Copyright (c) 2020 personal
 # <<licensetext>>
@@ -19,10 +19,11 @@ import queue,threading
 food = queue.Queue()
 def producer(name):
     count = 0
-    while count < 20:
+    while count < 15:
         time.sleep(random.randrange(3))
-        food.put(count)  # 在队列里放包子
-        print('工人 %s 生产了 %s 号包子并放在了盘子里.' % (name, count))
+        baozi = '%s-%s'%(name,count)
+        food.put(baozi)  # 在队列里放包子
+        print('工人 %s 生产了名为 %s 的包子并放在了盘子里.' % (name, baozi))
         count += 1
 '''
 #第1种消费者使用count来终止while循环
@@ -51,10 +52,12 @@ def consumer(name):
             print("盘子里没有包子了,顾客%s一共抢到了%s个包子"%(name, count))
             break
 p1 = threading.Thread(target=producer, args=('Anddy',))
+p2 = threading.Thread(target=producer, args=('Jimmy',))
 c1 = threading.Thread(target=consumer, args=('Tom',))
 c2 = threading.Thread(target=consumer, args=('Jaccob',))
-#当前py文件运行的时候会同时生成3个线程,1个生产者线程,2个消费者线程,
+#当前py文件运行的时候会同时生成4个线程,2个生产者线程,2个消费者线程,
 #他们之间,彼此没有先后关系,仅使用queue.Queue()来处理并协作作问题
 p1.start()
+p2.start()
 c1.start()
 c2.start()
