0a3419ddf0f9682a7bc4b14d649d11b8ad9181b2
anddy.liu <lqdflying@gmail.com>
2020-05-31 15:48:29 +0800

    完整的异步协程动态调用样例

--- Learning_Python/Basic/coroutine_adv.py
+++ Learning_Python/Basic/coroutine_adv.py
@@ -5,7 +5,7 @@
 # Author: anddy.liu
 # Contact: <lqdflying@gmail.com>
 # 
-# Last Modified: Wednesday May 27th 2020 8:20:45 pm
+# Last Modified: Sunday May 31st 2020 3:38:58 pm
 # 
 # Copyright (c) 2020 personal
 # <<licensetext>>
@@ -14,35 +14,60 @@
 # Date      	 By	Comments
 # ----------	---	----------------------------------------------------------
 ###
-# python协程只能运行在事件循环中，但是一旦事件循环运行，又会阻塞当前任务。所以只能在当前进程中再开一个线程，这个线程的主要任务是运行事件循环，
-# 就是event_loop,因为他是一个无限循环，会阻塞当前线程。放一个自己写的demo，注释写的很详细。
-# 另外还有一点需要注意，一个事件循环中不能运行另外一个事件循环。
-import asyncio
+import asyncio,time
 from threading import Thread
-  
-async def consumption(i):
-    while True:
-        print("我是第{}任务".format(i))
-        await asyncio.sleep(1)
 
-async def production_task():
-    i = 0
-    while True:
-        # 将consumption这个协程每秒注册一个到运行在线程中的循环，thread_loop每秒会获得一个一直打印i的无限循环任务
-        asyncio.run_coroutine_threadsafe(consumption(i), thread_loop)  # 注意：run_coroutine_threadsafe 这个方法只能用在运行在线程中的循环事件使用
-        await asyncio.sleep(1)  # 必须加await
-        i += 1
-  
-  
 def start_loop(loop):
-    #  运行事件循环， loop以参数的形式传递进来运行
     asyncio.set_event_loop(loop)
     loop.run_forever()
-  
-  
-thread_loop = asyncio.new_event_loop()  # 获取一个事件循环
-run_loop_thread = Thread(target=start_loop, args=(thread_loop,))  # 将次事件循环运行在一个线程中，防止阻塞当前主线程
-run_loop_thread.start()  # 运行线程，同时协程事件循环也会运行
-  
-advocate_loop = asyncio.get_event_loop()  # 将生产任务的协程注册到这个循环中
-advocate_loop.run_until_complete(production_task())  # 运行次循环
+
+
+async def stop_loop():
+    # event loop监控协程,目的是如果当前 event loop为空,则退出
+    while True:
+        if len(asyncio.all_tasks()) != 1:
+            print(f"当前event loop共有{len(asyncio.all_tasks())}个task在运行",)
+            await asyncio.sleep(3) #每3秒钟扫一遍当前的event loop,看看有几个任务
+            continue
+        else:
+            print("没有多于的task了,关闭整个事件循环")
+            break
+    a = asyncio.get_running_loop()
+    # print(a)
+    a.stop()
+
+async def cancel_me():
+    print(f"cancel_me(): before sleep at {time.strftime('%X')}")
+
+    try:
+        # Wait for 1 hour
+        await asyncio.sleep(3600)
+    except asyncio.CancelledError:
+        print(f"cancel_me(): cancel sleep at {time.strftime('%X')}")
+        raise
+    finally:
+        print(f"cancel_me(): after sleep at {time.strftime('%X')}")
+
+async def cancel_main():
+    # Create a "cancel_me" Task
+    task = asyncio.create_task(cancel_me())
+    print(f"返回task的名字at {time.strftime('%X')}: ",task.get_name())
+    # Wait for 1 second
+    await asyncio.sleep(5)
+    
+    task.cancel()
+    try:
+        await task
+    except asyncio.CancelledError:
+        print(f"main(): cancel_me is cancelled now at {time.strftime('%X')}")
+
+
+
+new_loop = asyncio.new_event_loop()
+t_run = Thread(target=start_loop, args=(new_loop,))
+t_run.start()
+
+for i in range(2):#协程开启十个
+    asyncio.run_coroutine_threadsafe(cancel_main(), new_loop)
+
+asyncio.run_coroutine_threadsafe(stop_loop(), new_loop)
