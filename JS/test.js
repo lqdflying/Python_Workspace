cdbd630fc2b23a13344c7b2c4149f9ac01603e66
anddy.liu <lqdflying@gmail.com>
2020-06-13 11:38:05 +0800

    2020-06-08 16:52:29

---
+++ JS/test.js
@@ -0,0 +1,18 @@
+/**
+ * File: test.js
+ * Project: e:\GitHub\Python_Workspace\JS
+ * Created Date: 2020-06-13  11:24:37
+ * Author: anddy.liu
+ * -----
+ * Last Modified: Sat Jun 13 2020
+ * Modified By: anddy.liu
+ * -----
+ * Copyright (c) 2020 personal
+ * ------------------------------------
+ * Javascript will save your soul!
+ */
+
+alert("今天蓝天白云");
+var a = prompt("请随便输入点什么东西吧");
+console.log(a);
+alert("我很高兴");
