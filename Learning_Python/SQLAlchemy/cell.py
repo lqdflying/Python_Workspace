69ccc9a371d48898f772f21355a87fe0ceffbff8
anddy.liu <lqdflying@gmail.com>
2020-12-17 18:24:28 +0800

    2020-12-17 18:24:26

---
+++ Learning_Python/SQLAlchemy/cell.py
@@ -0,0 +1,18 @@
+###
+# File: cell.py
+# Created Date: 2020-12-17
+# Author: anddy.liu
+# Contact: <lqdflying@gmail.com>
+# 
+# Last Modified: Thursday December 17th 2020 6:19:47 pm
+# 
+# Copyright (c) 2020 personal
+# <<licensetext>>
+# -----
+# HISTORY:
+# Date      	 By	Comments
+# ----------	---	----------------------------------------------------------
+###
+
+msg = "hello world"
+print(msg)
---
+++ Learning_Python/SQLAlchemy/ch06.py
@@ -0,0 +1,87 @@
+###
+# File: ch06.py
+# Created Date: 2020-12-17
+# Author: anddy.liu
+# Contact: <lqdflying@gmail.com>
+# 
+# Last Modified: Thursday December 17th 2020 5:25:20 pm
+# 
+# Copyright (c) 2020 personal
+# <<licensetext>>
+# -----
+# HISTORY:
+# Date      	 By	Comments
+# ----------	---	----------------------------------------------------------
+###
+
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Table, Column, Integer, Numeric, String, Boolean
+
+Base = declarative_base()
+
+class Cookie(Base):
+    __tablename__ = 'cookies'
+
+    cookie_id = Column(Integer(), primary_key=True)
+    cookie_name = Column(String(50), index=True)
+    cookie_recipe_url = Column(String(255))
+    cookie_sku = Column(String(55))
+    quantity = Column(Integer())
+    unit_cost = Column(Numeric(12, 2))
+
+# %% [markdown]
+# Cookie.__table__
+
+# %%
+from datetime import datetime
+from sqlalchemy import DateTime
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    user_id = Column(Integer(), primary_key=True)
+    username = Column(String(15), nullable=False, unique=True)
+    email_address = Column(String(255), nullable=False)
+    phone = Column(String(20), nullable=False)
+    password = Column(String(25), nullable=False)
+    created_on = Column(DateTime(), default=datetime.now)
+    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+
+
+# %%
+User.__table__
+
+
+# %%
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+class Order(Base):
+    __tablename__ = 'orders'
+    order_id = Column(Integer(), primary_key=True)
+    user_id = Column(Integer(), ForeignKey('users.user_id'))
+    shipped = Column(Boolean(), default=False)
+    user =  relationship("User", backref=backref('orders', order_by=id))
+
+
+class LineItems(Base):
+    __tablename__ = 'line_items'
+    line_items_id = Column(Integer(), primary_key=True)
+    order_id = Column(Integer(), ForeignKey('orders.order_id'))
+    cookie_id = Column(Integer(), ForeignKey('cookies.cookie_id'))
+    quantity = Column(Integer())
+    extended_cost = Column(Numeric(12, 2))
+    order = relationship("Order", backref=backref('line_items', order_by=line_items_id))
+    cookie = relationship("Cookie", uselist=False)
+
+
+# %%
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///:memory:')
+
+Base.metadata.create_all(engine)
+
+
