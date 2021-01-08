9bb9f78ae534e07ac01a99f2588a476ae7f9e4d5
anddy.liu <lqdflying@gmail.com>
2021-01-08 11:38:32 +0800

    2021-1-8 11:38:30

---
+++ Learning_Python/SQLAlchemy/ch14/Association Proxy - Part 1.py
@@ -0,0 +1,93 @@
+###
+# File: Association Proxy - Part 1.py
+# Created Date: 2021-01-08
+# Author: anddy.liu
+# Contact: <lqdflying@gmail.com>
+# 
+# Last Modified: Friday January 8th 2021 11:37:57 am
+# 
+# Copyright (c) 2021 personal
+# <<licensetext>>
+# -----
+# HISTORY:
+# Date      	 By	Comments
+# ----------	---	----------------------------------------------------------
+###
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///:memory:')
+
+Session = sessionmaker(bind=engine)
+
+
+# %%
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, Numeric, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+cookieingredients_table = Table('cookieingredients', Base.metadata,
+    Column('cookie_id', Integer, ForeignKey("cookies.cookie_id"),
+           primary_key=True),
+    Column('ingredient_id', Integer, ForeignKey("ingredients.ingredient_id"),
+           primary_key=True)
+)
+
+
+class Ingredient(Base):
+    __tablename__ = 'ingredients'
+    
+    ingredient_id = Column(Integer, primary_key=True)
+    name = Column(String(255), index=True)
+    
+    def __repr__(self):
+        return "Ingredient(name='{self.name}')".format(self=self)
+
+
+class Cookie(Base):
+    __tablename__ = 'cookies'
+
+    cookie_id = Column(Integer, primary_key=True)
+    cookie_name = Column(String(50), index=True)
+    cookie_recipe_url = Column(String(255))
+    cookie_sku = Column(String(55))
+    quantity = Column(Integer())
+    unit_cost = Column(Numeric(12, 2))
+    
+    ingredients = relationship("Ingredient", secondary=cookieingredients_table)
+        
+    def __repr__(self):
+        return "Cookie(cookie_name='{self.cookie_name}', "                        "cookie_recipe_url='{self.cookie_recipe_url}', "                        "cookie_sku='{self.cookie_sku}', "                        "quantity={self.quantity}, "                        "unit_cost={self.unit_cost})".format(self=self)
+ 
+
+Base.metadata.create_all(engine)
+
+
+# %%
+session = Session()
+cc_cookie = Cookie(cookie_name='chocolate chip', 
+                   cookie_recipe_url='http://some.aweso.me/cookie/recipe.html', 
+                   cookie_sku='CC01', 
+                   quantity=12, 
+                   unit_cost=0.50)
+flour = Ingredient(name='Flour')
+sugar = Ingredient(name='Sugar')
+egg = Ingredient(name='Egg')
+cc = Ingredient(name='Chocolate Chips')
+cc_cookie.ingredients.extend([flour, sugar, egg, cc])
+session.add(cc_cookie)
+session.flush()
+
+
+# %%
+[ingredient.name for ingredient in cc_cookie.ingredients]
+
+
---
+++ Learning_Python/SQLAlchemy/ch14/Hybrid Properties.py
@@ -0,0 +1,111 @@
+###
+# File: Hybrid Properties.py
+# Created Date: 2021-01-08
+# Author: anddy.liu
+# Contact: <lqdflying@gmail.com>
+# 
+# Last Modified: Friday January 8th 2021 11:37:26 am
+# 
+# Copyright (c) 2021 personal
+# <<licensetext>>
+# -----
+# HISTORY:
+# Date      	 By	Comments
+# ----------	---	----------------------------------------------------------
+###
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///:memory:')
+
+Session = sessionmaker(bind=engine)
+
+
+# %%
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, Numeric, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
+
+Base = declarative_base()
+
+
+class Cookie(Base):
+    __tablename__ = 'cookies'
+
+    cookie_id = Column(Integer, primary_key=True)
+    cookie_name = Column(String(50), index=True)
+    cookie_recipe_url = Column(String(255))
+    cookie_sku = Column(String(55))
+    quantity = Column(Integer())
+    unit_cost = Column(Numeric(12, 2))
+    
+    @hybrid_property
+    def inventory_value(self):
+        return self.unit_cost * self.quantity
+    
+    @hybrid_method
+    def bake_more(self, min_quantity):
+        return self.quantity < min_quantity
+        
+    def __repr__(self):
+        return "Cookie(cookie_name='{self.cookie_name}', "                        "cookie_recipe_url='{self.cookie_recipe_url}', "                        "cookie_sku='{self.cookie_sku}', "                        "quantity={self.quantity}, "                        "unit_cost={self.unit_cost})".format(self=self)
+ 
+
+Base.metadata.create_all(engine)
+
+
+# %%
+print(Cookie.inventory_value < 10.00)
+
+
+# %%
+print(Cookie.bake_more(12))
+
+
+# %%
+session = Session()
+cc_cookie = Cookie(cookie_name='chocolate chip', 
+                   cookie_recipe_url='http://some.aweso.me/cookie/recipe.html', 
+                   cookie_sku='CC01', 
+                   quantity=12, 
+                   unit_cost=0.50)
+dcc = Cookie(cookie_name='dark chocolate chip',
+             cookie_recipe_url='http://some.aweso.me/cookie/recipe_dark.html',
+             cookie_sku='CC02',
+             quantity=1,
+             unit_cost=0.75)
+mol = Cookie(cookie_name='molasses',
+             cookie_recipe_url='http://some.aweso.me/cookie/recipe_molasses.html',
+             cookie_sku='MOL01',
+             quantity=1,
+             unit_cost=0.80)
+session.add(cc_cookie)
+session.add(dcc)
+session.add(mol)
+session.flush()
+
+
+# %%
+dcc.inventory_value
+
+
+# %%
+dcc.bake_more(12)
+
+
+# %%
+from sqlalchemy import desc
+for cookie in session.query(Cookie).order_by(desc(Cookie.inventory_value)):
+    print('{:>20} - {:.2f}'.format(cookie.cookie_name, cookie.inventory_value))
+
+
+# %%
+for cookie in session.query(Cookie).filter(Cookie.bake_more(12)):
+    print('{:>20} - {}'.format(cookie.cookie_name, cookie.quantity))
+
+
